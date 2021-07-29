4915b1a3c5d72a71af8a641e2ceeabba7700d71f
abdulrahman emad <abdulrahman3mad@gmail.com>
2021-07-29 14:43:48 +0200

    change

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../model/user")
-const ejs = require("ejs")  
 
 //generic functions 
 function creatJWToken(res, user) {

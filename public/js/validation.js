7728c455caf705d6590adfd672e2fb179f8e390e
abdulrahman emad <abdulrahman3mad@gmail.com>
2021-08-03 12:50:40 +0200

    final version

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,8 +1,13 @@
 let email = document.getElementById("email")
 let password = document.getElementById("password")
 let form = document.querySelector("form")
-let password_regex_length = /[a-zA-Z]{1,}\w{5,}/;
-let password_message = document.querySelector(".passwordMessage")
+let passwordRegexLength = /[a-zA-Z]{1,}\w{5,}/;
+let passwordMessage = document.querySelector(".passwordMessage")
+let serverMessage = document.getElementById("serverMessage")
+
+
+window.addEventListener("load", () => {
+})
 
 password.addEventListener("input", (e) => {
     passwordAuth(password.value)
@@ -11,18 +16,18 @@ password.addEventListener("input", (e) => {
 function unvalidPasswordStyle(error_txt) {
     password.style.borderColor = "red";
     password.style.borderWidth = "2px";
-    password_message.textContent = error_txt;
+    passwordMessage.textContent = error_txt;
 }
 
 function passwordAuth(passwordInput) {
-    if (!password_regex_length.test(passwordInput)) {
+    if (!passwordRegexLength.test(passwordInput)) {
         unvalidPasswordStyle("Password should be at least six characters starting by an English letter")
         return false;
     }
 
     else {
         password.style.borderColor = "green";
-        password_message.textContent = '';
+        passwordMessage.textContent = '';
         return true;
     }
 }
